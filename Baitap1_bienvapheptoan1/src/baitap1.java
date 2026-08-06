06c3b84f59bb0c49ff58ec802945c997b554f09d
tienlmcc25tth-creator <tien.lm.cc25tth@ntu.edu.vn>
2026-08-06 22:27:14 +0700

    hoàn thiện bài 1: Biến và phép

---
+++ Baitap1_bienvapheptoan1/src/baitap1.java
@@ -0,0 +1,19 @@
+
+public class baitap1 {
+
+	public static void main(String[] args) {
+	//1. khai báo các biến
+		int a = 15;
+		int b = 4;
+		double c = 3.5;
+	//2. tính toán
+		int tong = a + b;
+		double thuong = a / c;
+		int phanDu = a % b;
+	//3. kết quả
+		System.out.println("Tổng:"+ tong);
+		System.out.println("Thương:"+ thuong + "\n");
+		System.out.println("Phần dư:"+ phanDu);
+	}
+
+}
