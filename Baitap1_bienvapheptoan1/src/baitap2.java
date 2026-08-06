a8d73036b69be706987c23da30f29aa9b1e6917a
tienlmcc25tth-creator <tien.lm.cc25tth@ntu.edu.vn>
2026-08-06 22:55:17 +0700

    đã làm xong bài tập 2:  chuyển đổi nhiệt độ

---
+++ Baitap1_bienvapheptoan1/src/baitap2.java
@@ -0,0 +1,13 @@
+
+public class baitap2 {
+
+	public static void main(String[] args) {
+		// chuyển từ  Celsius sang Fahrenheit
+		// công thưc: F = (C x 9/5) + 32
+		double celsius = 25.0;
+		double fahrenheit = (celsius * 9/5) + 32;
+		
+		System.out.println(celsius +"°C =" + fahrenheit + "°F");
+	}
+
+}
