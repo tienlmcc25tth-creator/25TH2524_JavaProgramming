fa986f292e87f145141d2c22e11dbd1f2b453c49
tienlmcc25tth-creator <tien.lm.cc25tth@ntu.edu.vn>
2026-08-16 19:37:05 +0700

    thêm mã nhập khai báo biến

--- Bai1_Nhapxuat_Tinhtong2524/src/TongHaiSo.java
+++ Bai1_Nhapxuat_Tinhtong2524/src/TongHaiSo.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
 
 public class TongHaiSo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Khai báo
+		double num1, num2, sum;
+		//khai báo đối tương Scanner
+		Scanner banPhim = new Scanner(System.in);
+		//in ra thông tin, mời User nhập số thứ nhất
+		System.out.print("mời nhập số thứ nhất: ");
+		num1 = banPhim.nextDouble();
+		
+		System.out.print("mời nhập số thứ hai: ");
+		num2 = banPhim.nextDouble();
 
 	}
 
